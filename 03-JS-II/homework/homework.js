39b732751c2556eed78ac4d365a3dbee0af363dd
[Amaurys-Castro] <[acastrodearco@gmail.com]>
2022-08-08 21:05:23 -0500

    pr

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -161,7 +161,7 @@ function esVerdadero(valor){
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 if(valor === true){
-  return "Soy verdadero”
+  return "Soy verdadero"
 }
   return "Soy falso"
 }
